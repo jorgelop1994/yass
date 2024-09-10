5bc223a97dd47c4c1928e212fe99bdbe1a58f33f
jorgelop1994 <jorgelop1994@gmail.com>
2024-09-10 12:42:28 -0600

    refactor(security): Improve system security measures and configure SSH for two-factor authentication

--- 3-googleAuthenticator.sh
+++ 3-googleAuthenticator.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
+# Function to display an error message and exit
+error_exit() {
+    echo "$1" >&2
+    exit 1
+}
+
 # Check if the script is running as root or with sudo
-if [ "$(id -u)" -eq 0 ]; then
-  echo "This script should not be run as root or with sudo. Please run it as a regular user."
-  exit 1
+if [[ "$(id -u)" -eq 0 ]]; then
+    error_exit "This script should not be run as root or with sudo. Please run it as a regular user."
 fi
 
-# Install and configure Google Authenticator for 2FA
+# Install Google Authenticator for 2FA
 echo "Installing Google Authenticator..."
-sudo apt install libpam-google-authenticator -y
+sudo apt update && sudo apt install libpam-google-authenticator -y || error_exit "Failed to install Google Authenticator."
 
-echo "Configuring SSH to use Google Authenticator..."
 # Modify PAM to use Google Authenticator
-sudo tee -a /etc/pam.d/sshd > /dev/null <<EOL
+echo "Configuring SSH to use Google Authenticator..."
+if ! sudo tee -a /etc/pam.d/sshd > /dev/null <<EOL
 
 # Google Authenticator Authentication
 auth required pam_google_authenticator.so
 
 EOL
+then
+    error_exit "Failed to configure PAM for Google Authenticator."
+fi
 
 # Configure SSH to require 2FA in addition to a password
-sudo sed -i 's/ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config
-sudo sed -i 's/#AuthenticationMethods/AuthenticationMethods/' /etc/ssh/sshd_config
-sudo sed -i '/AuthenticationMethods/c\AuthenticationMethods publickey,keyboard-interactive' /etc/ssh/sshd_config
+echo "Configuring SSH for two-factor authentication..."
+sudo sed -i 's/^ChallengeResponseAuthentication no/ChallengeResponseAuthentication yes/' /etc/ssh/sshd_config || error_exit "Failed to enable ChallengeResponseAuthentication."
+sudo sed -i 's/^#\?AuthenticationMethods.*/AuthenticationMethods publickey,keyboard-interactive/' /etc/ssh/sshd_config || error_exit "Failed to set AuthenticationMethods."
 
-echo "Restarting SSH to apply changes... Remember, your new SSH port is 2222"
-sudo systemctl restart sshd
+# Restart SSH to apply changes
+echo "Restarting SSH to apply changes..."
+sudo systemctl restart sshd || error_exit "Failed to restart SSH service."
 
-# Run google-authenticator for the current user
+# Run Google Authenticator setup for the current user
 echo "Setting up Google Authenticator for the current user..."
-google-authenticator
+if ! google-authenticator; then
+    error_exit "Failed to set up Google Authenticator for the current user."
+fi
 
 echo "Google Authenticator has been successfully configured for the current user."
